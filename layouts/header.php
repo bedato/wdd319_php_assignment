4cefbeb0a081d0c20be4a064bca59b717d4d4e79
Vedat Demiri <v.demiri1997@gmail.com>
2020-05-24 22:18:52 +0200

    added database data on main header and rec. cards, searchbar is done

--- layouts/header.php
+++ layouts/header.php
@@ -1,7 +1,23 @@
-<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-        <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-        <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+<?php
+
+$topStory = "SELECT * FROM `posts` WHERE top_story=1";
+
+$resStory = mysqli_query($conn, $topStory);
+
+if ($resStory === false) {
+    echo 'MYSQL Fehler ' . mysqli_info($conn);
+}
+
+$getTopStory = mysqli_fetch_all($resStory, MYSQLI_ASSOC);
+
+?>
+<?php foreach ($getTopStory as $datensatz) { ?>
+    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+            <h1 class="display-4 font-italic"><?php echo $datensatz['title']; ?></h1>
+            <p class="lead my-3"><?php echo $datensatz['content']; ?></p>
+            <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+        </div>
     </div>
-</div>
+
+<?php } ?>
