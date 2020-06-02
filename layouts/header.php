409f701bfba3ebad9e62ddb2230ff6c0b8a383eb
Vedat Demiri <v.demiri1997@gmail.com>
2020-06-02 22:53:24 +0200

    some basic changes on the front page: color palette change and some minor adjustments

--- layouts/header.php
+++ layouts/header.php
@@ -12,11 +12,11 @@ $getTopStory = mysqli_fetch_all($resStory, MYSQLI_ASSOC);
 
 ?>
 <?php foreach ($getTopStory as $datensatz) { ?>
-    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+    <div class="jumbotron p-3 my-3 p-md-5 text-black rounded bg-light">
         <div class="col-md-6 px-0">
             <h1 class="display-4 font-italic"><?php echo $datensatz['title']; ?></h1>
             <p class="lead my-3"><?php echo $datensatz['content']; ?></p>
-            <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+            <a class="btn btn-secondary btn-lg" href="index.php?page=register" role="button">Register to read more</a>
         </div>
     </div>
 
--- layouts/html/welcome.html.php
+++ layouts/html/welcome.html.php
@@ -1,9 +1,10 @@
 <div class="col-md-8 blog-main">
 
-    <div class="blog-post">
-        <h2 class="blog-post-title">Welcome to my Blog</h2>
-        <p class="blog-post-meta">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae fugiat nulla, quo, similique voluptate sint, vero laudantium doloremque laboriosam commodi non exercitationem aspernatur! Cupiditate molestias ab voluptatum, unde repellendus error, sint ex quisquam mollitia cum iusto! Tempora fugiat officia facere.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quae provident, itaque ipsa quam repellat quibusdam inventore est labore, omnis aperiam, minus ipsam. Quam consequuntur maxime molestias itaque expedita laborum, iusto explicabo vero similique facere nulla earum numquam sit, quis facilis sunt hic ad aperiam eveniet enim nostrum quas voluptates.</p>
+    <div>
+        <h2 class="pb-3">Welcome to my Blog</h2>
+        <p class="py-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae fugiat nulla, quo, similique voluptate sint, vero laudantium doloremque laboriosam commodi non exercitationem aspernatur! Cupiditate molestias ab voluptatum, unde repellendus error, sint ex quisquam mollitia cum iusto! Tempora fugiat officia facere.</p>
+        <p class="py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quae provident, itaque ipsa quam repellat quibusdam inventore est labore, omnis aperiam, minus ipsam. Quam consequuntur maxime molestias itaque expedita laborum, iusto explicabo vero similique facere nulla earum numquam sit, quis facilis sunt hic ad aperiam eveniet enim nostrum quas voluptates.</p>
+        <p class="py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quae provident, itaque ipsa quam repellat quibusdam inventore est labore, omnis aperiam, minus ipsam. Quam consequuntur maxime molestias itaque expedita laborum, iusto explicabo vero similique facere nulla earum numquam sit, quis facilis sunt hic ad aperiam eveniet enim nostrum quas voluptates.</p>
         <hr>
     </div>
 
